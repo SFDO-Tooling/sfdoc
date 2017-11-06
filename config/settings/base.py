6535fbc72b4315f517cad83fc65d85fedd1321a1
Chris Polcyn <cpolcyn@salesforce.com>
2017-11-06 15:34:52 -0800

    add django_rq to installed apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,6 +49,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'django_rq',  # django-rq
     'storages',  # django-storages
 ]
 
