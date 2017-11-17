bea0d544b7ecc47877c97021a50202088e1c7307
Chris Polcyn <cpolcyn@salesforce.com>
2017-11-17 13:56:43 -0800

    use python dict as default to env.json()

--- config/settings/base.py
+++ config/settings/base.py
@@ -285,26 +285,24 @@ RQ_QUEUES = {
     },
 }
 
-default_html_whitelist = """
-{
-    "a": ["href"],
-    "body": [],
-    "div": ["class"],
-    "h1": [],
-    "h2": [],
-    "h3": [],
-    "h4": [],
-    "h5": [],
-    "h6": [],
-    "head": [],
-    "html": [],
-    "img": ["src"],
-    "li": [],
-    "meta": ["content", "name"],
-    "p": [],
-    "ul": []
+default_html_whitelist = {
+    'a': ['href'],
+    'body': [],
+    'div': ['class'],
+    'h1': [],
+    'h2': [],
+    'h3': [],
+    'h4': [],
+    'h5': [],
+    'h6': [],
+    'head': [],
+    'html': [],
+    'img': ['src'],
+    'li': [],
+    'meta': ['content', 'name'],
+    'p': [],
+    'ul': []
 }
-"""
 HTML_WHITELIST = env.json('HTML_WHITELIST', default=default_html_whitelist)
 
 HTML_EXTENSIONS = ('.htm', '.html')
