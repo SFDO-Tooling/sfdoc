d128abdfc4710a5c7f053fe16a78884e5aa82324
Chris Polcyn <cpolcyn@salesforce.com>
2017-11-28 17:46:00 -0800

    fix up Salesforce JWT auth

--- sfdoc/publish/salesforce.py
+++ sfdoc/publish/salesforce.py
@@ -1,48 +1,37 @@
+from calendar import timegm
 from datetime import datetime
-from datetime import timedelta
-from http import HTTPStatus
 
 from django.conf import settings
+import json
 import jwt
 import requests
-
-
-def get_access_token(connected_app):
-    """Use JWT to get OAuth access token."""
-    payload = {
-        'iss': settings.SALESFORCE_CLIENT_ID,
-        'sub': settings.SALESFORCE_USERNAME,
-        'aud': settings.SALESFORCE_AUTH_SITE,
-        'exp': datetime.utcnow() + timedelta(minutes=1),
-    }
-    encoded_jwt = jwt.encode(
-        payload,
-        settings.SALESFORCE_CLIENT_SECRET,
-        algorithm='RS256',
-    )
-    url = settings.SALESFORCE_AUTH_SITE + '/services/oauth2/token'
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    params = {
-        'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-        'assertion': encoded_jwt,
-    }
-    response = requests.post(
-        url,
-        data=encoded_jwt,
-        headers=headers,
-        params=params,
-    )
-    if response.status_code >= HTTPStatus.BAD_REQUEST:
-        raise Exception('Status {}: {}'.format(
-            response.status_code,
-            response.content,
-        ))
-    return response.body['access_token']
+from simple_salesforce import Salesforce
 
 
 def get_salesforce_api():
     """Get an instance of the Salesforce REST API."""
+    payload = {
+        'alg': 'RS256',
+        'iss': settings.SALESFORCE_CLIENT_ID,
+        'sub': settings.SALESFORCE_USERNAME,
+        'aud': settings.SALESFORCE_AUTH_SITE,
+        'exp': timegm(datetime.utcnow().utctimetuple()),
+    }
+    encoded_jwt = jwt.encode(
+        payload,
+        settings.SALESFORCE_JWT_PRIVATE_KEY,
+        algorithm='RS256',
+    )
+    url = settings.SALESFORCE_AUTH_SITE + '/services/oauth2/token'
+    data = {
+        'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        'assertion': encoded_jwt,
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.post(url, data=data, headers=headers)
+    response.raise_for_status()
+    response_data = json.loads(response.text)
     return Salesforce(
-        instance=settings.SALESFORCE_INSTANCE,
-        session_id=get_access_token(),
+        instance_url=response_data['instance_url'],
+        session_id=response_data['access_token'],
     )
