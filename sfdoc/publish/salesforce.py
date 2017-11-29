250e56c6ec5e40cedef20a2612e9a466dbc83d95
Chris Polcyn <cpolcyn@salesforce.com>
2017-11-29 14:53:31 -0800

    cleanup

--- sfdoc/publish/salesforce.py
+++ sfdoc/publish/salesforce.py
@@ -25,10 +25,6 @@ def get_salesforce_api():
         settings.SALESFORCE_JWT_PRIVATE_KEY,
         algorithm='RS256',
     )
-    kwargs = {
-        'client_id': 'sfdoc',
-        'sandbox': settings.SALESFORCE_SANDBOX,
-    }
     data = {
         'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
         'assertion': encoded_jwt,
@@ -38,8 +34,9 @@ def get_salesforce_api():
     response = requests.post(url=auth_url, data=data, headers=headers)
     response.raise_for_status()
     response_data = json.loads(response.text)
-    kwargs.update({
-        'instance_url': response_data['instance_url'],
-        'session_id': response_data['access_token'],
-    })
-    return Salesforce(**kwargs)
+    return Salesforce(
+        instance_url=response_data['instance_url'],
+        session_id=response_data['access_token'],
+        sandbox=settings.SALESFORCE_SANDBOX,
+        client_id='sfdoc',
+    )
