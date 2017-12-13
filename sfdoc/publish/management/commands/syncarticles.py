0d05ab64891f3e81aa60030ea9260d28b1e97d16
Chris Polcyn <cpolcyn@salesforce.com>
2017-12-13 14:50:09 -0800

    fix up article sync command

--- sfdoc/publish/management/commands/syncarticles.py
+++ sfdoc/publish/management/commands/syncarticles.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
+from sfdoc.publish.models import Article
 from sfdoc.publish.salesforce import get_salesforce_api
 
 
@@ -10,27 +11,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         sf = get_salesforce_api()
         result = sf.query_all(
-            "SELECT Id,KnowledgeArticleId,Title,UrlName,{}".format(
+            "SELECT KnowledgeArticleId,UrlName,Title,Summary,{}".format(
                 settings.SALESFORCE_ARTICLE_BODY_FIELD
             ) +
-            " FROM KnowledgeArticleVersion" +
-            " WHERE ArticleType = '{}'".format(
-                settings.SALESFORCE_ARTICLE_TYPE
-            ) +
-            " AND PublishStatus = 'Online'"
+            " FROM {}".format(settings.SALESFORCE_ARTICLE_TYPE) +
+            " WHERE PublishStatus='Online' AND language='en_US'"
         )
         for record in result['records']:
-            Article.objects.update_or_create(
+            article, created = Article.objects.update_or_create(
                 salesforce_id=record['KnowledgeArticleId'],
                 defaults={
-                    'body': record[settings.SALESFORCE_ARTICLE_BODY_FIELD],
-                    'title': record['Title'],
                     'url_name': record['UrlName'],
+                    'title': record['Title'],
+                    'summary': record['Summary'],
+                    'body': record[settings.SALESFORCE_ARTICLE_BODY_FIELD],
                 },
             )
             self.stdout.write(self.style.SUCCESS(
                 'Successfully synced article {} "{}"'.format(
-                    record['KnowledgeArticleId'],
-                    record['Title']
+                    article.salesforce_id,
+                    article.title,
                 )
             ))
