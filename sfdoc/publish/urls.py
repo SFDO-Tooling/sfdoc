f1ec82c203d933df55b3adac643277bb740ee5c0
Chris Polcyn <cpolcyn@salesforce.com>
2018-01-29 14:12:09 -0800

    clean up URL order (no effect in this case)

--- sfdoc/publish/urls.py
+++ sfdoc/publish/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     url(
-        regex=r'^(?P<easydita_bundle_id>\d+)/$',
-        view=views.bundle_status,
-        name='bundle_status',
-    ),
-    url(
         regex=r'^$',
         view=views.queue_status,
         name='queue_status',
+    ),
+    url(
+        regex=r'^(?P<easydita_bundle_id>\d+)/$',
+        view=views.bundle_status,
+        name='bundle_status',
     ),
     url(
         regex=r'^webhook/$',
