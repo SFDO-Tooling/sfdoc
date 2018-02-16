98a2b9d6698bb4187fddc9534b80be13666320b5
Chris Polcyn <cpolcyn@salesforce.com>
2018-02-15 17:02:57 -0800

    move Salesforce OAuth mock into test file

--- sfdoc/publish/tests/test_salesforce.py
+++ sfdoc/publish/tests/test_salesforce.py
@@ -1,19 +1,22 @@
+from urllib.parse import urljoin
+
 from django.conf import settings
 import responses
 from test_plus.test import TestCase
 
 from ..salesforce import Salesforce
-from .utils import mock_salesforce_auth
 
 
 class TestSalesforce(TestCase):
 
-    def setUp(self):
-        self.instance_url = 'https://testinstance.salesforce.com'
-
     @responses.activate
-    def test__init__(self):
+    def test_init(self):
         """Get API to a Salesforce org."""
-        mock_salesforce_auth(self.instance_url)
+        url = urljoin(settings.SALESFORCE_LOGIN_URL, 'services/oauth2/token')
+        json = {
+            'instance_url': 'https://testinstance.salesforce.com',
+            'access_token': 'abc123',
+        }
+        responses.add('POST', url=url, json=json)
         salesforce = Salesforce()
         self.assertEqual(len(responses.calls), 1)
