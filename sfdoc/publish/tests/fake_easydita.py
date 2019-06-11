72a94ee35a8c020e2f75e55bb4dc157a9b23e8ed
Paul Prescod <pprescod@salesforce.com>
2019-06-11 03:15:18 -0700

    Additional test data

--- sfdoc/publish/tests/fake_easydita.py
+++ sfdoc/publish/tests/fake_easydita.py
@@ -1,4 +1,4 @@
-# https://salesforce.easydita.com/api/get/db/organizations/salesforce/published/77b09a40-819a-11e9-b28c-42010af00002/BundleA_2019-05-30T01-02Z.zip
+# output-uuid# https://salesforce.easydita.com/api/get/db/organizations/salesforce/published/77b09a40-819a-11e9-b28c-42010af00002/BundleA_2019-05-30T01-02Z.zip
 fake_webhook_body_doc_A = {
     "event_id": "dita-ot-publish-complete",
     "event_data": {
@@ -43,10 +43,21 @@ ditamap_A_V2_titles = [
     "Article B2",
 ]
 
+ditamap_A_V5_titles = [
+    "Article A3! Updated",
+    "Article A1",
+    "Test Documentation: Bundle A",
+]
+
 ditamap_B_titles = [
     "Article 1, Bundle B",
     "Article 3, Bundle B",
     "Article 2, Bundle B",
+    "Test Documentation: Bundle B",
+]
+
+ditamap_B_V3_titles = [
+    "Article 1, Bundle B",
     "Test Documentation: Bundle B",
 ]
 
@@ -73,6 +84,16 @@ fake_webhook_body_doc_A_V4 = {
     "resource_id": "77b09a40-819a-11e9-b28c-42010af00002",
 }
 
+fake_webhook_body_doc_A_V5 = {
+    "event_id": "dita-ot-publish-complete",
+    "event_data": {
+        "publish-result": "success",
+        "output-uuid": "3953bb10-8992-11e9-b28c-42010af00002",
+        "description": "V5 no image",
+    },
+    "resource_id": "77b09a40-819a-11e9-b28c-42010af00002",
+}
+
 # https://salesforce.easydita.com/api/get/db/organizations/salesforce/published/bf0080a0-8270-11e9-b28c-42010af00002/BundleB_2019-05-30T00-51Z.zip
 fake_webhook_body_doc_B = {
     "event_id": "dita-ot-publish-complete",
@@ -85,6 +106,19 @@ fake_webhook_body_doc_B = {
     },
     "resource_id": "bf0080a0-8270-11e9-b28c-42010af00002",
 }
+
+fake_webhook_body_doc_b_V3 = {
+    "event_id": "dita-ot-publish-complete",
+    "event_data": {
+        "publish-result": "success",
+        "output-uuid": "de6045b0-8b95-11e9-b28c-42010af00002",
+        "description": "Version 3: Bundle B, Article 2 removed",
+        "processor": "dita-open-toolkits/organization-dita-ot-2.2.5-custom.jcmconnect",
+        "date-time": "2019-06-10T15:39:00.884Z",
+    },
+    "resource_id": "bf0080a0-8270-11e9-b28c-42010af00002",
+}
+
 
 preloaded_article_titles = [
     "foundationConnect Documentation",
