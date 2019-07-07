2d96c408f28266144178bdb7bc904dfcc5e452c1
Paul Prescod <pprescod@salesforce.com>
2019-07-07 07:48:57 -0700

    More realistic factory.

--- sfdoc/publish/tests/factories.py
+++ sfdoc/publish/tests/factories.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import factory
 from .. import models
 
@@ -5,3 +7,6 @@ from .. import models
 class BundleFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Bundle
+    easydita_id = factory.Faker('first_name')
+    easydita_resource_id = factory.Faker('last_name')
+    time_queued = factory.LazyFunction(datetime.now)
