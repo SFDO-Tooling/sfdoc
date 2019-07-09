9e59b53fd8244a7f98adfb1031597526953eb7c9
Paul Prescod <pprescod@salesforce.com>
2019-07-09 06:58:57 -0700

    Un-blackify for now.

--- sfdoc/publish/tests/utils.py
+++ sfdoc/publish/tests/utils.py
@@ -19,7 +19,7 @@ rootdir = os.path.abspath(os.path.join(__file__, "../../../.."))
 
 def create_test_html(url_name, title, summary, body):
     """Create string of HTML to use for testing."""
-    s = """
+    s = '''
     <html>
     <head>
       <meta name="UrlName" content="{}">
@@ -35,7 +35,7 @@ def create_test_html(url_name, title, summary, body):
       <div class="{}">{}</div>
     </body>
     </html>
-    """.format(
+    '''.format(
         url_name,
         summary,
         settings.ARTICLE_AUTHOR,
@@ -50,84 +50,116 @@ def create_test_html(url_name, title, summary, body):
 def gen_article(n):
     """Create article fields using a number."""
     return {
-        "id": n,
-        "filename": "test{}.html".format(n),
-        "url_name": "test-{}-url-name".format(n),
-        "title": "Test {} Title".format(n),
-        "summary": "Test {} Summary".format(n),
-        "body": (
-            "Test article content\n<br/>\n" '<img src="../images/test-image.png"/>\n'
+        'id': n,
+        'filename': 'test{}.html'.format(n),
+        'url_name': 'test-{}-url-name'.format(n),
+        'title': 'Test {} Title'.format(n),
+        'summary': 'Test {} Summary'.format(n),
+        'body': (
+            'Test article content\n<br/>\n'
+            '<img src="../images/test-image.png"/>\n'
         ),
     }
 
 
 def mock_create_article(instance_url, kav_id):
-    url = "{}/services/data/v{}/sobjects/{}/".format(
-        instance_url, settings.SALESFORCE_API_VERSION, settings.SALESFORCE_ARTICLE_TYPE
+    url = '{}/services/data/v{}/sobjects/{}/'.format(
+        instance_url,
+        settings.SALESFORCE_API_VERSION,
+        settings.SALESFORCE_ARTICLE_TYPE,
     )
-    responses.add("POST", url=url, json={"id": kav_id})
+    responses.add('POST', url=url, json={'id': kav_id})
 
 
 def mock_create_draft(instance_url, ka_id, kav_id):
     url = (
-        "{}/services/data/v{}" "/knowledgeManagement/articleVersions/masterVersions"
-    ).format(instance_url, settings.SALESFORCE_API_VERSION)
-    responses.add("POST", url=url, status=HTTPStatus.CREATED, json={"id": str(kav_id)})
+        '{}/services/data/v{}'
+        '/knowledgeManagement/articleVersions/masterVersions'
+    ).format(
+        instance_url,
+        settings.SALESFORCE_API_VERSION,
+    )
+    responses.add(
+        'POST',
+        url=url,
+        status=HTTPStatus.CREATED,
+        json={'id': str(kav_id)},
+    )
 
 
 def mock_bundle_download(url, articles):
     """Mock the response from easyDITA to provide the bundle."""
     zip_buff = BytesIO()
-    with ZipFile(zip_buff, mode="w") as f_zip:
+    with ZipFile(zip_buff, mode='w') as f_zip:
         for a in articles:
-            f_zip.writestr(
-                a["filename"],
-                create_test_html(a["url_name"], a["title"], a["summary"], a["body"]),
-            )
+            f_zip.writestr(a['filename'], create_test_html(
+                a['url_name'],
+                a['title'],
+                a['summary'],
+                a['body'],
+            ))
     responses.add(
-        "GET", url=url, body=zip_buff.getvalue(), content_type="application/zip"
+        'GET',
+        url=url,
+        body=zip_buff.getvalue(),
+        content_type='application/zip',
     )
 
 
 def mock_publish_draft(instance_url, kav_id):
     url = (
-        "{}/services/data/v{}/knowledgeManagement/articleVersions"
-        + "/masterVersions/{}"
-    ).format(instance_url, settings.SALESFORCE_API_VERSION, kav_id)
-    responses.add("PATCH", url=url, status=HTTPStatus.NO_CONTENT)
+        '{}/services/data/v{}/knowledgeManagement/articleVersions' +
+        '/masterVersions/{}'
+    ).format(
+        instance_url,
+        settings.SALESFORCE_API_VERSION,
+        kav_id,
+    )
+    responses.add('PATCH', url=url, status=HTTPStatus.NO_CONTENT)
 
 
-def mock_query(instance_url, url_name, publish_status, fields=None, return_val=None):
+def mock_query(
+    instance_url,
+    url_name,
+    publish_status,
+    fields=None,
+    return_val=None,
+):
     """Mock KAV query to Salesforce org."""
     if not fields:
-        fields = ["Id"]
-    query_s = urlencode(
-        {
-            "q": (
-                "SELECT {} FROM {} WHERE UrlName='{}' "
-                "AND PublishStatus='{}' AND language='en_US'"
-            ).format(
-                ",".join(fields),
-                settings.SALESFORCE_ARTICLE_TYPE,
-                url_name,
-                publish_status,
-            )
-        }
+        fields = ['Id']
+    query_s = urlencode({
+        'q': (
+            "SELECT {} FROM {} WHERE UrlName='{}' "
+            "AND PublishStatus='{}' AND language='en_US'"
+        ).format(
+            ','.join(fields),
+            settings.SALESFORCE_ARTICLE_TYPE,
+            url_name,
+            publish_status,
+        ),
+    })
+    url = '{}/services/data/v{}/query/?{}'.format(
+        instance_url,
+        settings.SALESFORCE_API_VERSION,
+        query_s,
     )
-    url = "{}/services/data/v{}/query/?{}".format(
-        instance_url, settings.SALESFORCE_API_VERSION, query_s
+    responses.add(
+        'GET',
+        url=url,
+        match_querystring=True,
+        json=return_val,
     )
-    responses.add("GET", url=url, match_querystring=True, json=return_val)
 
 
 def mock_update_draft(instance_url, kav_id):
     url = urljoin(
         instance_url,
-        "services/data/v{}/sobjects/{}/{}".format(
+        'services/data/v{}/sobjects/{}/{}'.format(
             settings.SALESFORCE_API_VERSION, settings.SALESFORCE_ARTICLE_TYPE, kav_id
         ),
     )
-    responses.add("PATCH", url=url, status=HTTPStatus.NO_CONTENT)
+    responses.add('PATCH', url=url, status=HTTPStatus.NO_CONTENT)
 
 
 ORIGINIT = TemporaryDirectory.__init__
