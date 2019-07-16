76a9d29bb395c4c5f63bc835f4553e8a3ef733ea
Paul Prescod <pprescod@salesforce.com>
2019-07-16 12:04:37 -0700

    Some webhook stuff.

--- scripts/fake_webhook.py
+++ scripts/fake_webhook.py
@@ -18,8 +18,19 @@ def jsonify(output_UUID, resource_UUID):
 
 
 url = "https://sfdoc-staging.sfdc.sh/publish/webhook/"
+
+# Bundle A
 output_UUID = "726c4040-a68c-11e9-b28c-42010af00002"
-resource_UUID = "7b62ee90-2886-11e8-8740-42010af00002"
+resource_UUID = "77b09a40-819a-11e9-b28c-42010af00002"
+
+# Bundle B
+output_UUID = "211d8cc0-8275-11e9-b28c-42010af00002"
+resource_UUID = "bf0080a0-8270-11e9-b28c-42010af00002"
+
+# Bundle B V3
+output_UUID = "de6045b0-8b95-11e9-b28c-42010af00002"
+resource_UUID = "bf0080a0-8270-11e9-b28c-42010af00002"
+
 result = requests.post(url, json=jsonify(output_UUID, resource_UUID))
 
 pprint(result)
