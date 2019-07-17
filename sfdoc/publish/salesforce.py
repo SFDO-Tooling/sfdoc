4b39b09e9cae022a8870b6dd7bc31d8a4e2ddfd0
Ryan Foster <ryan.foster@salesforce.com>
2019-07-17 13:03:58 -0600

    Reusable salesforce api instantiation

--- sfdoc/publish/salesforce.py
+++ sfdoc/publish/salesforce.py
@@ -20,6 +20,39 @@ from logging import getLogger
 query_logger = getLogger("query_str")
 
 
+def get_salesforce_api():
+    """Get an instance of the Salesforce REST API."""
+    url = settings.SALESFORCE_LOGIN_URL
+    if settings.SALESFORCE_SANDBOX:
+        url = url.replace('login', 'test')
+    payload = {
+        'alg': 'RS256',
+        'iss': settings.SALESFORCE_CLIENT_ID,
+        'sub': settings.SALESFORCE_USERNAME,
+        'aud': url,
+        'exp': timegm(datetime.utcnow().utctimetuple()),
+    }
+    encoded_jwt = jwt.encode(
+        payload,
+        settings.SALESFORCE_JWT_PRIVATE_KEY,
+        algorithm='RS256',
+    )
+    data = {
+        'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        'assertion': encoded_jwt,
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    auth_url = urljoin(url, 'services/oauth2/token')
+    response = requests.post(url=auth_url, data=data, headers=headers)
+    response.raise_for_status()  # maybe VPN or auth problem!
+    response_data = response.json()
+    return SimpleSalesforce(
+        instance_url=response_data['instance_url'],
+        session_id=response_data['access_token'],
+        sandbox=settings.SALESFORCE_SANDBOX,
+        version=settings.SALESFORCE_API_VERSION,
+        client_id='sfdoc',
+    )
 class SalesforceArticles:
     """A docset-scoped or unscoped view of Salesforce Knowledge articles"""
 
