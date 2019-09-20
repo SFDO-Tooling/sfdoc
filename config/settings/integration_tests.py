4f5b4ea2c3b80c0d3bcce8e7a5dc952ab46bf1c7
Paul Prescod <pprescod@salesforce.com>
2019-09-20 09:08:36 -0700

    Clean up cruft.

--- config/settings/integration_tests.py
+++ config/settings/integration_tests.py
@@ -41,15 +41,6 @@ INSTALLED_APPS += ["debug_toolbar"]
 
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
-DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-    "SHOW_TEMPLATE_CONTEXT": True,
-}
-
-# django-extensions
-# ------------------------------------------------------------------------------
-INSTALLED_APPS += ["django_extensions"]
-
 # TESTING
 # ------------------------------------------------------------------------------
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
