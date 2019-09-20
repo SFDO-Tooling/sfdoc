6907bf124839732adce59601579a5b6c45609618
Paul Prescod <pprescod@salesforce.com>
2019-09-20 09:15:00 -0700

    Clean up more cruft.

--- config/settings/integration_tests.py
+++ config/settings/integration_tests.py
@@ -34,11 +34,6 @@ CACHES = {
     }
 }
 
-# django-debug-toolbar
-# ------------------------------------------------------------------------------
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-INSTALLED_APPS += ["debug_toolbar"]
-
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 # TESTING
