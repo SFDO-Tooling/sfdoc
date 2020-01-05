27968670dc41e0f42b4aba554f46a6c8b63fff55
Paul Prescod <pprescod@salesforce.com>
2020-01-04 20:05:06 -0800

    Updates to environment variables etc.

--- utility/heroku-release.py
+++ utility/heroku-release.py
@@ -1,11 +1,6 @@
 import os
 
-if os.environ.get(HEROKU_APP_NAME, "").startswith("sfdoc-review-apps-"):
-    os.system("pip install --upgrade -r test/requirements.txt")
-    os.system("pytest")
-
-# Todo:
-
-# check for HEROKU_APP_NAME . startswith(sfdoc-review-apps-)
-# then run `pip install --upgrade test/requirements.txt`
-# then run pytest with all of the environment variables
+if os.environ.get("HEROKU_APP_NAME", "").startswith("sfdoc-review-apps-"):
+    assert not os.system("pip install --upgrade -r test/requirements.txt")
+    os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.integration_tests"
+    assert not os.system("pytest")
