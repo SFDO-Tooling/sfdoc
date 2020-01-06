231b1ff11b064be3e15aa9acc40a312f0bcf0529
Paul Prescod <pprescod@salesforce.com>
2020-01-06 09:29:51 -0800

    Add documentation about conftest.

--- sfdoc/conftest.py
+++ sfdoc/conftest.py
@@ -3,6 +3,12 @@ import pytest
 import environ
 from django.conf import settings
 
+# this recipe convinces pytest-sjango to use the pre-existing 
+# database instead of creating a new one. Heroku apps seem
+# to not have permissions to create databases on the fly.
+
+# https://pytest-django.readthedocs.io/en/latest/database.html#using-an-existing-external-database-for-tests
+
 if os.environ.get("HEROKU_APP_NAME", "").startswith("sfdoc-review-apps-"):
     env = environ.Env()
     @pytest.fixture(scope='session')
